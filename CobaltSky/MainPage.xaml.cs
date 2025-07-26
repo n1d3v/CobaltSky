77a9e9c9f70916caf764cab894afc095ce2eb8b5
patricktbp <135556230+n1d3v@users.noreply.github.com>
2025-07-26 20:42:55 +0100

    WRONG FILES OOPS

--- CobaltSky/MainPage.xaml.cs
+++ CobaltSky/MainPage.xaml.cs
@@ -1,48 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+﻿using System.Collections.Generic;
+using Microsoft.Phone.Controls;
 
 namespace CobaltSky
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public partial class MainPage : PhoneApplicationPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
 
-            this.NavigationCacheMode = NavigationCacheMode.Required;
+            var postItems = new List<PostControl>
+            {
+                new PostControl
+                {
+                    AuthorName = "patricktbp",
+                    AuthorPostDate = "12:34pm | 7/26/2025",
+                    PostText = "Test of how posts look.",
+                    LikeCount = "6.7K",
+                    CommentCount = "382",
+                    RepostCount = "761"
+                }
+            };
+            PostsView.ItemsSource = postItems;
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        public class PostControl
         {
-            // TODO: Prepare page for display here.
+            // Author related strings
+            public string AuthorName { get; set; }
+            public string AuthorPostDate { get; set; }
 
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+            // Post related strings
+            public string PostText { get; set; }
+
+            // Metric related strings
+            public string LikeCount { get; set; }
+            public string CommentCount { get; set; }
+            public string RepostCount { get; set; }
         }
     }
 }
